3198330d7c25ccda10fd6d4c75503fc3de3b4046
RestaBuilds <resta@gmod-rus.ru>
2016-12-27 18:36:35 +0300

    27.12.16

---
+++ createhtaccess.bat
@@ -0,0 +1,43 @@
+
+:: =======================================
+:: = BAT BY _RESTA_ | THANK LIPRIKONS    =
+:: = TO IDEA | _RESTA_ TO DO BAT :)      =
+:: =======================================
+
+@echo off :: ECHO OFF
+color fc :: COLOR SET
+echo WARNING!!! .htaccess file is not created by (PTP - New - FILE), and then rename it to be an error :: WARNING
+echo 5 Sec to create .htaccess :: TEXT
+@ping -n 1 -w 1000 1.0.0.0>nul :: TIMER 
+cls :: CLEAR TEXT TO CONSOLE 
+echo WARNING!!! .htaccess file is not created by (PTP - New - FILE), and then rename it to be an error
+echo 4 Sec to create .htaccess
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+echo WARNING!!! .htaccess file is not created by (PTP - New - FILE), and then rename it to be an error
+echo 3 Sec to create .htaccess
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+echo WARNING!!! .htaccess file is not created by (PTP - New - FILE), and then rename it to be an error
+echo 2 Sec to create .htaccess
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+echo WARNING!!! .htaccess file is not created by (PTP - New - FILE), and then rename it to be an error
+echo 1 Sec to create .htaccess
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+@echo /*Created by CreateHtacces.bat*/>.htaccess :: CREATE FILE IN TEXT
+color f1 
+echo INFO!!! Created!
+echo 3 Sec to exit
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+echo INFO!!! Created!
+echo 2 Sec to exit
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+echo INFO!!! Created!
+echo 3 Sec to exit
+@ping -n 1 -w 1000 1.0.0.0>nul
+cls
+exit :: EXIT
